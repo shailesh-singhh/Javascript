5eae019f57ba993317ea5fa648fe92ab1a8a50dd
Shailesh singh <singhshailesh2901@gmail.com>
2023-11-22 07:49:37 +0530

    learning variables and its types

---
+++ first.js
@@ -0,0 +1,29 @@
+console.log("Apna college");
+let firstName = "Shailesh singh";
+let clas =  "b.tech";
+let x = null;
+let y = undefined;
+let age = 22;
+let price = 343.4;
+console.log(firstName);
+console.log(clas);
+console.log(x);
+console.log(y);
+console.log(age);
+console.log(price);
+const student ={
+fullName : "Rahul Kumar",
+age : 19,
+cgpa : 9.78,
+isPass : true,
+};
+console.log(student);
+const profile = {
+    useraName : "Shradhakhapra",
+    isVerified : true,
+    posts : 195,
+    followers : "569k",
+    following : 4,
+    fullName : "Shradha Khapra",
+    about : "ENTREPRENEUR",
+};
