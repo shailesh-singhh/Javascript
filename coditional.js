7fc8ee7fb988c5d3852480c6a4fd05b0c17a6b29
Shailesh singh <singhshailesh2901@gmail.com>
2023-11-23 09:58:57 +0530

    learning conditional staements

---
+++ coditional.js
@@ -0,0 +1,23 @@
+//let num = prompt("Enter a number :");
+//if(num % 3 === 0){
+  //  console.log("Multiple of 3");
+//}
+//else{
+  //  console.log("Not a multiple of 3");
+//};
+let grade = prompt("Enter your  grade :");
+if(grade>=80 && grade<=100){
+    console.log("A");
+}
+else if(grade >= 70 && grade<80){
+    console.log("B");
+}
+else if(grade >= 60 && grade < 70){
+    console.log("C");
+}
+else if(grade >= 50 && grade < 60){
+    console.log("D");
+}
+else{
+    console.log("F");
+};
