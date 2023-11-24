89d8ade8de1ea91849c7a6b53c0fd8921f09fbb9
Shailesh singh <singhshailesh2901@gmail.com>
2023-11-24 22:49:43 +0530

    loops practice

---
+++ loops.js
@@ -0,0 +1,73 @@
+// Enter number from 1 to 10
+//for(let i = 1 ;i <= 10; i++){
+ //   console.log(i);
+//};
+// code to print sum of first n natural number
+//let n = prompt("Enter the number till which you want the sum :");
+//let sum = 0;
+//for(let i = 1; i <= n ;i++){
+//    sum+=i;
+   
+//};
+//console.log(sum);
+//console.log("loop ended"); 
+
+//-------------------------------------//
+//using while loop
+//let i = 1;
+//while(i<=5){
+ //   console.log(i);
+   // i++;
+//};
+
+//---------------------------------//
+ 
+//  USING DO - WHILE LOOP //
+//let i = 1;
+//do{
+//    console.log(i);
+//    i++;
+//}
+//while(i<=10);
+
+//----------------------------------//
+
+//  using FOR - OF loop             //
+
+//let str = "JAVASCRIPT";
+
+//let size = 0;
+//for(let i of str) {
+//    console.log(i);
+//    size++;
+//};
+
+//console.log("String size :" , size);
+
+// ------------------------------------//
+
+// using for in loop // 
+
+//let student = {
+ //   fullName : "Rahul Kumar ",
+ //   age    : 20,
+ //   cgpa   : 8.9,
+  //  ispass : true ,
+//};
+
+//for(let key in student){
+//    console.log("key = " ,key ," value =" ,student.key);
+//};
+
+// ------ EVEN NUMBER  from 0 to 100 ------------
+
+for(let i = 0 ;i <= 100 ; i++){
+
+if(i%2==0){
+    console.log(i);
+}
+};
+
+
+ 
+
