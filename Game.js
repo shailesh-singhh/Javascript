87af1c0a1d14d0f285b91e76f9136d9c61a53fbd
Shailesh singh <singhshailesh2901@gmail.com>
2023-11-25 19:02:08 +0530

    some changes

--- Game.js
+++ Game.js
@@ -1,6 +1,7 @@
 let num = 25;
 let entered = prompt(" Enter the guessed number :");
-if(num != entered){
+while(num != entered){
+    entered = prompt("You entered a wrong number . guess again -");
 
 }
 console.log("Congratulations you entered correct number  :-)");
