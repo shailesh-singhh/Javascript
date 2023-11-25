6365561c6268c66f0ccfbec57a266fd7d5410c83
Shailesh singh <singhshailesh2901@gmail.com>
2023-11-25 20:10:58 +0530

    learning strings

---
+++ string.js
@@ -0,0 +1,15 @@
+//let obj = {
+  //  item :"pen",
+  //  price : 10,
+//};
+ //let output = `The cost of ${obj.item} is ${obj.price} rupees`;
+ //console.log(output);
+ //let str = "Apna\tCollege";
+ //console.log(str);
+
+ 
+ //console.log(str.toUpperCase());
+ 
+let str = prompt("Enter your Name ");
+console.log(`${"@"}${str}${str.length}`);
+
