6920aee605cf676098f2ea0c0fef2f4531ad64f9
Shailesh singh <singhshailesh2901@gmail.com>
2023-11-26 23:09:15 +0530

    functions

---
+++ function.js
@@ -0,0 +1,21 @@
+//function myFunction(){
+   // console.log("Welcome to Apna college");
+//    console.log("We are learning Javascript");
+//}
+
+//myFunction();
+
+//---------------FUNCTION TO ADD TWO NUMBER----------//
+// let x = prompt("Enter the first number ");
+// let y = prompt("Enter second number");
+// let z = prompt("Enter third number ");
+function addition(x,y,z){
+    console.log(x+y+z);
+}
+addition(34,34,54);
+  
+//------------Multiplication Function ---------------//
+
+function mul(a ,b){
+    return a * b ;
+}
