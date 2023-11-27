c2f83dd891568a18b2d57f4f23b8becd34b38878
Shailesh singh <singhshailesh2901@gmail.com>
2023-11-27 21:48:28 +0530

    some changes

--- DOM.js
+++ DOM.js
@@ -1,3 +1,11 @@
 alert("ARE YOU SURE TO OPEN THE WINDOW");
 console.dir(document.body);
 console.log(document.body);
+
+let heading = document.getElementById("heading");
+console.log(heading);
+let para = document.getElementsByClassName("para");
+console.log(para);
+
+let para2 = document.getElementsByClassName("p");
+console.log(para2);
