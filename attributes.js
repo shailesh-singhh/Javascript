864d9c0d7775b8bb3075c92eade726ca4717837f
Shailesh singh <singhshailesh2901@gmail.com>
2023-12-02 22:51:23 +0530

    attributes and style

---
+++ attributes.js
@@ -0,0 +1,12 @@
+let div1  = document.querySelector("div");
+console.log(div1);
+let val = div1.getAttribute("id");
+console.log(val);
+
+let div2 = document.querySelector("p");
+console.log(div2);
+
+let val2 = div2.getAttribute("name");
+console.log(val2);
+
+console.log(div1.setAttribute("id" , "NewId"));
---
+++ practice.js
@@ -0,0 +1,3 @@
+let heading = document.querySelector("h2");
+console.log(heading);
+    console.log(heading.innerText=heading.innerText + "From Apna College students ");
