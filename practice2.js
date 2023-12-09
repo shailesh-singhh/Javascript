2dce378f7153e19825ee85f79232cd004a37ddb3
Shailesh singh <singhshailesh2901@gmail.com>
2023-12-09 10:47:52 +0530

    Practice Queation on js Dom manipulation

---
+++ practice2.js
@@ -0,0 +1,12 @@
+// ----------------------FIRST QUESTION SOLUTION-------------------------
+
+// let newBtn = document.createElement("button");
+// newBtn.innerText="Click Me";
+// newBtn.style.backgroundColor="red";
+// newBtn.style.color="white";
+// let div = document.querySelector("div");
+// div.before(newBtn);
+
+//--------------------------SECOND QUESTION SOLUTION-----------------------
+
+let para = document.querySelector("p");
