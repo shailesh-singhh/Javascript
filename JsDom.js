3dd7a1336e6f013939d16598b42cfa5f9b9b9735
Shailesh singh <singhshailesh2901@gmail.com>
2023-12-09 10:11:47 +0530

    js tutorial creating and adding new elements in a class

--- JsDom.js
+++ JsDom.js
@@ -1,2 +1,20 @@
-let high = document.querySelector("h1");
-console.log(high.setAttribute("class" ,"NewClass"));
+// let high = document.querySelector("h1");
+// console.log(high.setAttribute("class" ,"NewClass"));
+// let newButton = document.createElement("button");
+// newButton.innerText="Click Me";
+// console.log(newButton);
+
+// let div = document.querySelector("div");
+
+//div.prepend(newButton);
+// div.after(newButton);
+//div.before(newButton);
+let newHeading = document.createElement("h1");
+newHeading.innerHtml = "<i>I am new </i>";
+newHeading.innerText="Hey ! I am New ";
+let body =document.querySelector("body");
+body.prepend(newHeading);
+let para = document.querySelector("p");
+para.remove();
+// let img =document.querySelector("img");
+// img.remove();
